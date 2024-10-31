7ead9b892e01a5d210c3c39fc0d1fc2e68508cce
冬喃 <xuenhao.xeh@antgroup.com>
2024-10-31 13:54:36 +0800

    add deployment handler unit test

--- controller/module_deployment_controller/module_deployment_controller_test.go
+++ controller/module_deployment_controller/module_deployment_controller_test.go
@@ -1,11 +1,25 @@
 package module_deployment_controller
 
-import "testing"
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
 
 func TestDeploymentHandler(t *testing.T) {
-	c := &ModuleDeploymentController{}
+	c := &ModuleDeploymentController{
+		runtimeStorage: NewRuntimeInfoStore(),
+		updateToken:    make(chan interface{}),
+	}
 
 	c.deploymentAddHandler(nil)
+
+	assert.Equal(t, 0, len(c.runtimeStorage.peerDeploymentMap))
+
 	c.deploymentUpdateHandler(nil, nil)
+
+	assert.Equal(t, 0, len(c.runtimeStorage.peerDeploymentMap))
+
 	c.deploymentDeleteHandler(nil)
+
+	assert.Equal(t, 0, len(c.runtimeStorage.peerDeploymentMap))
 }
