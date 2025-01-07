8e04a465162b587949a766ab62550518bad6f01c
gaosaroma <gaosaroma@gmail.com>
2025-01-07 11:58:36 +0800

    fix: add defensive checks for resp of error status

--- module_tunnels/koupleless_http_tunnel/ark_service/ark_service.go
+++ module_tunnels/koupleless_http_tunnel/ark_service/ark_service.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"github.com/koupleless/arkctl/v1/service/ark"
 	"github.com/koupleless/module_controller/common/zaplogger"
+	"net/http"
 
 	"github.com/go-resty/resty/v2"
 )
@@ -38,6 +39,12 @@ func (h *Service) InstallBiz(ctx context.Context, req InstallBizRequest, baseIP 
 		return
 	}
 
+	if resp.StatusCode() != http.StatusOK {
+		err = errors.New(fmt.Sprintf("response status: %d", resp.StatusCode()))
+		logger.Error(err, "installBiz request failed")
+		return
+	}
+
 	if err = json.Unmarshal(resp.Body(), &response); err != nil {
 		logger.Error(err, fmt.Sprintf("Unmarshal InstallBiz response: %s", resp.Body()))
 		return
@@ -61,6 +68,12 @@ func (h *Service) UninstallBiz(ctx context.Context, req UninstallBizRequest, bas
 
 	if resp == nil {
 		err = errors.New("received nil response from the server")
+		logger.Error(err, "uninstall request failed")
+		return
+	}
+
+	if resp.StatusCode() != http.StatusOK {
+		err = errors.New(fmt.Sprintf("response status: %d", resp.StatusCode()))
 		logger.Error(err, "uninstall request failed")
 		return
 	}
@@ -92,6 +105,12 @@ func (h *Service) QueryAllBiz(ctx context.Context, baseIP string, port int) (res
 		return
 	}
 
+	if resp.StatusCode() != http.StatusOK {
+		err = errors.New(fmt.Sprintf("response status: %d", resp.StatusCode()))
+		logger.Error(err, "queryAllBiz request failed")
+		return
+	}
+
 	if err = json.Unmarshal(resp.Body(), &response); err != nil {
 		logger.Error(err, fmt.Sprintf("Unmarshal QueryAllBiz response: %s", resp.Body()))
 		return
@@ -115,6 +134,12 @@ func (h *Service) Health(ctx context.Context, baseIP string, arkletPort int) (re
 
 	if resp == nil {
 		err = errors.New("received nil response from the server")
+		logger.Error(err, "health request failed")
+		return
+	}
+
+	if resp.StatusCode() != http.StatusOK {
+		err = errors.New(fmt.Sprintf("response status: %d", resp.StatusCode()))
 		logger.Error(err, "health request failed")
 		return
 	}
